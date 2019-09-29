97c897b1143ed1448693245b5c070bbf3731cf2f
Jason Pickens <jasonpickensnz@gmail.com>
2019-09-29 17:51:43 +1300

    Create jandals.py

---
+++ jandals/jandals.py
@@ -0,0 +1,10 @@
+import argparse
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog="jandals",
+        description="Bootstraps a new development environment with no prerequisites.",
+    )
+    parser.parse_args()
+    print("Can you handle the jandle?")
